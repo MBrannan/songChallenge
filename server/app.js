6ce631b4d86e76fd71c25c892b52dec6576108bd
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-08 15:57:46 -0600

    initial commit to my repo

--- server/app.js
+++ server/app.js
@@ -2,15 +2,38 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+//puts post request body data and store it on req.body
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', process.env.PORT || 3000);
 
+var songs = [
+   {
+      artist: "Bruce Springsteen",
+      title: "Born in the U.S.A."
+   }
+];
+
+//Routes
+app.get('/songs', function(req, res) {
+  console.log('handling get request for songs');
+  res.send(songs);
+});
+
+app.post('/songs', function(req, res) {
+  console.log("REQ body: ", req.body);
+  var newSong = req.body;
+  songs.push(newSong);
+  res.sendStatus(201);
+});
 
 // static file routing
 app.get('/*', function(req, res) {
   var file = req.params[0] || '/views/index.html';
   console.log(file);
-  
+
   res.sendFile(path.join(__dirname, './public/', file));
   // /public/views/index.html
 });
